222b0aa96278ac483790039b0184538d45701b40
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-05-31 14:04:09 +0200

    started with the routing the nav

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import Nav from "@/lib/components/nav";
+
+export default function Home() {
+    return (
+        <>
+            <Nav />
+            <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+        </>
+    );
+}
